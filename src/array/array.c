628454a3d898b06fd6385e6222e656e116b44212
Liam Crewe <liam.j.crewe@gmail.com>
2016-11-04 15:38:50 +0000

    Reordered functions in array.c|h
    Makes more sense to keep the malloc/free functions together, then the
    search functions below.

--- src/array/array.c
+++ src/array/array.c
@@ -22,23 +22,6 @@ double **createTwoDDoubleArray(const int dimension)
     return rows;
 }
 
-int twoDIntArrayContains(
-    const int value,
-    int ** const array,
-    const int dimension
-)
-{
-    for(int row = 0; row < dimension; row++) {
-        for(int col = 0; col < dimension; col++) {
-            if (array[row][col] == value) {
-                return 1;
-            }
-        }
-    }
-
-    return 0;
-}
-
 void freeTwoDIntArray(int **array, const int dimension)
 {
     for(int row = 0; row < dimension; row++) {
@@ -55,6 +38,23 @@ void freeTwoDDoubleArray(double **array, const int dimension)
     }
 
     free(array);
+}
+
+int twoDIntArrayContains(
+    const int value,
+    int ** const array,
+    const int dimension
+)
+{
+    for(int row = 0; row < dimension; row++) {
+        for(int col = 0; col < dimension; col++) {
+            if (array[row][col] == value) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 int intArraySearch(const int value, int * const array, const int dimension)
--- src/array/array.h
+++ src/array/array.h
@@ -1,10 +1,10 @@
 int **createTwoDIntArray(const int dimension);
 double **createTwoDDoubleArray(const int dimension);
+void freeTwoDIntArray(int **array, const int dimension);
+void freeTwoDDoubleArray(double **array, const int dimension);
 int twoDIntArrayContains(
     const int value,
     int ** const array,
     const int dimension
 );
-void freeTwoDIntArray(int **array, const int dimension);
-void freeTwoDDoubleArray(double **array, const int dimension);
 int intArraySearch(const int value, int * const array, const int dimension);
