70b30c3686ec8770c589eb41b9fbac964f79221b
Liam Crewe <liam.j.crewe@gmail.com>
2016-11-04 15:34:55 +0000

    Adding comments to debug.h and debug.c

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * Print a two dimensional array of doubles to the screen.
+ *
+ * @param array     The array to print
+ * @param dimension The dimension of the array to print
+ */
 void print2dDoubleArray(double ** const array, const int dimension)
 {
     for (int row = 0; row < dimension; ++row) {
--- src/debug/debug.h
+++ src/debug/debug.h
@@ -1,1 +1,7 @@
+/**
+ * Print a two dimensional array of doubles to the screen.
+ *
+ * @param array     The array to print
+ * @param dimension The dimension of the array to print
+ */
 void print2dDoubleArray(double ** const array, const int dimension);
