a9aa6b7c9ca335d45e0f09802b07fd3be2397113
Liam Crewe <liam.j.crewe@gmail.com>
2016-11-04 15:17:13 +0000

    Adding comments to isEven function

--- src/utility/utility.c
+++ src/utility/utility.c
@@ -1,4 +1,11 @@
-const int isEven(int value)
+/**
+ * Checks if an integer value is even.
+ *
+ * @param  value The integer value to check
+ *
+ * @return       1 if even, 0 otherwise
+ */
+const int isEven(const int value)
 {
     return value % 2 == 0;
 }
--- src/utility/utility.h
+++ src/utility/utility.h
@@ -1,1 +1,8 @@
+/**
+ * Checks if an integer value is even
+ *
+ * @param  value The integer value to check
+ *
+ * @return       1 if even, 0 otherwise
+ */
 const int isEven(const int value);
