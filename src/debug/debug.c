ea5eae65007389a23bfe693c64d74dd38cd30ba1
Liam Crewe <liam.j.crewe@gmail.com>
2016-11-04 15:33:38 +0000

    Removing now unused print2dArray function

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-void print2dArray(int ** const array, const int dimension)
-{
-    for (int row = 0; row < dimension; row++) {
-        for (int col = 0; col < dimension; col++) {
-            printf("%10d ", array[row][col]);
-        }
-        puts("");
-    }
-}
-
 void print2dDoubleArray(double ** const array, const int dimension)
 {
     for (int row = 0; row < dimension; ++row) {
--- src/debug/debug.h
+++ src/debug/debug.h
@@ -1,2 +1,1 @@
-void print2dIntArray(int ** const array, const int dimension);
 void print2dDoubleArray(double ** const array, const int dimension);
