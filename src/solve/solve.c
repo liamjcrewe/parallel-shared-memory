9c9756d3c699b843080d894590d1c25d23e5f41b
Liam Crewe <liam.j.crewe@gmail.com>
2016-11-05 17:09:25 +0000

    Adding lots of comments and some const keywords
    Comments are neccessary to explain how readability has been sacrificed
    for speed and efficiency. Also added const keyword to some variables,
    particularly in struct typedef.
    More comments to be added in future commits.

--- src/solve/solve.c
+++ src/solve/solve.c
@@ -1,3 +1,32 @@
+/**
+ * Algorithm background:
+ * ---------------------
+ *
+ * This file contains most of the complicated logic to solve the problem using
+ * a parallel algorithm. It does, in places, sacrifice code readability for
+ * speed and efficiency, hence this long comment. To give some background to
+ * the algorithm, the input two dimensional array should be viewed as follows:
+ *
+ *   X X X X X          X X X X X X
+ *   X E O E X          X E O E O X
+ *   X O E O X    AND   X O E O E X
+ *   X E O E X          X E O E O X
+ *   X X X X X          X O E O E X
+ *                      X X X X X X
+ *
+ * where E = point where row and column indices add to an even value,
+ *       O = point where row and column indicies add to an odd value,
+ *       X = edge value i.e. is a fixed point.
+ *
+ * Using this view it is clear that all Es can be worked on in parallel, and
+ * likewise for all Os. This is the basis of the parallel algorithm; work on as
+ * many points that are independent of one another as possible.
+ *
+ * Much of the complex logic is in finding out which point give to the next
+ * thread to work on. See moveToNext function comment for further details on
+ * this.
+ */
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,17 +35,40 @@
 #include "../array/array.h"
 #include "../utility/utility.h"
 
+// struct to pass multiple arguments to thread callback function
 typedef struct {
-    double **values;
-    int row;
-    int col;
-    double precision;
-    int *threadAvailableFlag;
-    pthread_mutex_t *threadAvailableFlagLock;
-    int *valuesSolvedPoint;
-    int *wereValuesModified;
+    double ** const values; // The two dimensional array of values being solved
+    const int row; // The row to update in the thread
+    const int col; // The column to update in the thread
+    const double precision; // The precision to work to
+    int * const threadAvailableFlag; // Flag - is a given thread available
+    pthread_mutex_t * const threadAvailableFlagLock; // Lock on availability
+                                                     // flag to restrict
+                                                     // concurrent access
+    int * const valuesSolvedPoint; // Flag - is the given point solved
+    int * const wereValuesModified; // Flag - was value updated (i.e new value
+                                    // calculated and changed by more than the
+                                    // precision)
 } ThreadArgs;
 
+/**
+ * Sets the given square two dimensional array to the following style
+ * (of given size):
+ *    1 1 1 1 1
+ *    1 0 0 0 1
+ *    1 0 0 0 1
+ *    1 0 0 0 1
+ *    1 1 1 1 1
+ *
+ * That is, edges set to 1, and inner values set to 0. This array represents
+ * which values have been solved in function solve, and hence needs to be reset
+ * every time one of the values is updated. We can only say we are done when a
+ * full pass of the values array in solve has been done without any values
+ * being updated.
+ *
+ * @param solvedArray The two dimensional array to set to the above style
+ * @param dimension   The dimension of the two dimensional array given
+ */
 static void resetSolvedArray(int ** const solvedArray, const int dimension)
 {
     for(int row = 0; row < dimension; row++) {
@@ -33,9 +85,35 @@ static void resetSolvedArray(int ** const solvedArray, const int dimension)
     }
 }
 
+/**
+ * Update row and col to point to the next point that should be calculated.
+ *
+ * This is complicated as the position of the next point (when moving to the
+ * next row) depends on the dimension of the given input array. Below shows
+ * an example of both an odd and even dimension grid.
+ *
+ *   X X X X X          X X X X X X
+ *   X E O E X          X E O E O X
+ *   X O E O X    AND   X O E O E X
+ *   X E O E X          X E O E O X
+ *   X X X X X          X O E O E X
+ *                      X X X X X X
+ *
+ * If the current point is second or third last point, we know that we should
+ * move to the next row. We always move two spaces at a time (see grids above)
+ * and the edges of the grid are fixed. The column we start at on the next row
+ * depends on the current column and the dimension of the grid.
+ *
+ * @param row       The row of the current point, to be updated to row of next
+ *                  point. May not change
+ * @param col       The column of the current point, to be updated to the
+ *                  column of the next point
+ * @param dimension The dimension of the two dimensional array we are solving
+ */
 static void moveToNext(int * const row, int * const col, const int dimension)
 {
     if (*col == dimension - 2) {
+        // if even dimension, start at first column, otherwise start at second
         *col = isEven(dimension) ? 1 : 2;
         *row += 1;
 
@@ -43,6 +121,7 @@ static void moveToNext(int * const row, int * const col, const int dimension)
     }
 
     if (*col == dimension - 3) {
+        // if even dimension, start at second column, otherwise start at first
         *col = isEven(dimension) ? 2 : 1;
         *row += 1;
 
@@ -54,11 +133,34 @@ static void moveToNext(int * const row, int * const col, const int dimension)
     return;
 }
 
+/**
+ * Check if a point is that last 'even' point to be calculated (see comment at
+ * top of this file for definition of what an even/E point is). That is, the
+ * bottom right corner of the grid (excluding the fixed edge points).
+ *
+ * @param  row       The row of the point
+ * @param  col       The column of the point
+ * @param  dimension The dimension of the two dimensional array we are solving
+ *
+ * @return           1 if last even point, 0 otherwise
+ */
 static int isLastEvenPoint(const int row, const int col, const int dimension)
 {
     return (row == dimension - 2) && (col == dimension - 2);
 }
 
+/**
+ * Check if a point is that last 'odd' point to be calculated (see comment at
+ * top of this file for definition of what an odd/O point is). That is, one
+ * column before the bottom right corner of the grid (excluding the fixed edge
+ * points).
+ *
+ * @param  row       The row of the point
+ * @param  col       The column of the point
+ * @param  dimension The dimension of the two dimensional array we are solving
+ *
+ * @return           1 if last odd point, 0 otherwise
+ */
 static int isLastOddPoint(const int row, const int col, const int dimension)
 {
     return (row == dimension - 2) && (col == dimension - 3);
@@ -77,7 +179,7 @@ static void *endThread(
 }
 
 static void *updateValue(
-    double **values,
+    double ** const values,
     const int row,
     const int col,
     const double precision,
@@ -181,7 +283,7 @@ void solve(
         // Second condition:
         // reverse version of XOR
         if (!valuesSolvedArray[row][col]) {
-            ThreadArgs args = {
+            const ThreadArgs args = {
                 .values = values,
                 .row = row,
                 .col = col,
@@ -195,7 +297,7 @@ void solve(
             pthread_mutex_lock(&threadsAvailableLocks[tId]);
 
             // create the thread
-            pthread_create(&tIds[tId], NULL, updateValueProxy, &args);
+            pthread_create(&tIds[tId], NULL, updateValueProxy, (void *)&args);
             // set lock to unavailable
             threadsAvailable[tId] = 0;
 
